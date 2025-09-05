842740483085f6ec20c42a43eefda58fb0a87f8c
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-09-05 19:22:44 -0400

    Problem #2 second test

--- Problem2.js
+++ Problem2.js
@@ -1,14 +1,21 @@
 const p2test = {};
 
 p2test.test = function () {
-for (i = 0; i < 20; i++) {
+for (let i = 0; i < 20; i++) {
     const block = document.createElement("div");
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+    const row = document.getElementById("row");
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    while (r === g && g === b) {
+      r = Math.floor(Math.random() * 256);
+      g = Math.floor(Math.random() * 256);
+      b = Math.floor(Math.random() * 256);
+    }
     block.style.width = "40px";
     block.style.height = "40px";
     block.style.display = "inline-block"; // single row
     block.style.backgroundColor = `rgb(${r},${g},${b})`;
+    row.appendChild(block);
   }
 }

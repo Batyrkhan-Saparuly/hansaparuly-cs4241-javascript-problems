55c9777aa23def293d70e4d711fba968ea7b565c
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-09-05 19:58:33 -0400

    Problem #3 Test

---
+++ Problem3.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("letter");
+input.addEventListener("input", function(event) {
+    document.createElement("h1");
+    const value = event.target.value;
+    if (value !== "") {h1.innerText = value};
+});

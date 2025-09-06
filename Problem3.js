d2bafd2aefbb6d74d0885815c00ee7d3a00b0eea
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-09-05 20:09:56 -0400

    Problem #3 second test

--- Problem3.js
+++ Problem3.js
@@ -3,4 +3,6 @@ input.addEventListener("input", function(event) {
     document.createElement("h1");
     const value = event.target.value;
     if (value !== "") {h1.innerText = value};
+    document.body.appendChild(h1);
+    event.target.value = "";
 });

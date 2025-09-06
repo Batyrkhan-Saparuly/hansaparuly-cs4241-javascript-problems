1928eed2953b07d9141daeba6df03ac39d2a4e4d
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-09-05 20:16:41 -0400

    Problem #3 third test

--- Problem3.js
+++ Problem3.js
@@ -1,8 +1,8 @@
 const input = document.getElementById("letter");
 input.addEventListener("input", function(event) {
-    document.createElement("h1");
+    const h1block = document.createElement("h1");
     const value = event.target.value;
-    if (value !== "") {h1.innerText = value};
-    document.body.appendChild(h1);
+    if (value !== "") {h1block.innerText = value};
+    document.body.appendChild(h1letter);
     event.target.value = "";
 });

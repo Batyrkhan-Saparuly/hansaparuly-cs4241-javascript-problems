b1f257d41cacb828f69b2b98a1e16e277e21e7da
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-09-05 20:53:55 -0400

    Problem #4 fix

--- Problem4.js
+++ Problem4.js
@@ -1,12 +1,16 @@
 const btnfour = document.getElementById("btnfour");
+btnfour.style.width = "40px";
+btnfour.style.height = "40px";
 btnfour.addEventListener("click", function swap(event) {
     oldBtn = event.currentTarget;
-    oldBtn.remove();
     const newBtn = document.createElement("button");
     newBtn.type = "button";
+    newBtn.style.width = "40px";
+    newBtn.style.height = "40px";
     function randomLabel () {
         return "btn-" + Math.random().toString(16).slice(2,6);
     }
     newBtn.textContent = randomLabel();
     newBtn.addEventListener("click", swap);
+    parent.replaceChild(newBtn, oldBtn);
 });

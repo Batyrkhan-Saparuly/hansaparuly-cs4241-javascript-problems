91c50dc50950de5cac26b0f74ac2b7bd86d00649
Batyrkhan Saparuly <157060867+Han-Saparuly@users.noreply.github.com>
2025-09-05 20:22:27 -0400

    Problem #3 fix attempt

--- Problem3.js
+++ Problem3.js
@@ -2,7 +2,7 @@ const input = document.getElementById("letter");
 input.addEventListener("input", function(event) {
     const h1block = document.createElement("h1");
     const value = event.target.value;
-    if (value !== "") {h1block.innerText = value};
-    document.body.appendChild(h1letter);
+    if (value !== "") {h1block.innerText = value[0]};
+    h1letter.appendChild(h1block);
     event.target.value = "";
 });
